7245054a851a5a70a28384c34a9983cf703c9c4d
Eric Beach <ebeach@google.com>
2013-04-06 14:14:21 -0400

    Starter project

---
+++ Combinations.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: Combinations.cpp
+ * ----------------------
+ * Name: Eric Beach
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Combinations problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main() {
+    // [TODO: fill in the code]
+    return 0;
+}
