92d6e458b65fd7fc86542a984bfd69f3d78e59b8
Eric Beach <ebeach@google.com>
2013-04-06 14:38:55 -0400

    Basic working version

--- Combinations.cpp
+++ Combinations.cpp
@@ -11,7 +11,44 @@
 #include "console.h"
 using namespace std;
 
+int c(int n, int k) {
+    if (n == 0 || k == n || k == 0) {
+        return 1;
+    } else {
+        return c(n - 1, k - 1) + c(n - 1, k);
+    }
+}
+
+bool assertEquals(int expected, int received) {
+    if (expected == received) {
+        return true;
+    } else {
+        cout << "TEST FAILURE -- Expected: " << expected << ", Received: " <<
+          received << endl;
+        return false;
+    }
+}
+
+void testC() {
+    assertEquals(1, c(0, 0));
+    assertEquals(1, c(1, 0));
+    assertEquals(1, c(1, 1));
+    assertEquals(1, c(3, 0));
+    assertEquals(3, c(3, 1));
+    assertEquals(3, c(3, 2));
+    assertEquals(1, c(3, 3));
+    assertEquals(1, c(7, 0));
+    assertEquals(7, c(7, 1));
+    assertEquals(21, c(7, 2));
+    assertEquals(35, c(7, 3));
+    assertEquals(35, c(7, 4));
+    assertEquals(21, c(7, 5));
+    assertEquals(7, c(7, 6));
+    assertEquals(1, c(7, 7));
+}
+
 int main() {
-    // [TODO: fill in the code]
+    testC();
+    cout << c(3, 1) << endl;
     return 0;
 }
